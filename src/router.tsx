e901af3c1257dc287f508016356e4c167157e4dc
gzwirtes4-wq <gzwirtes4@gmail.com>
2026-09-19 13:58:55 -0300

    [nexable] Remove named export conflitante de routeTree em router.tsx que

--- src/router.tsx
+++ src/router.tsx
@@ -2,8 +2,6 @@ import { QueryClient } from "@tanstack/react-query";
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-export { routeTree } from "./routeTree.gen";
-
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
